dfd3406718b2bcc0dc3e767ec0a13ecc05c2ca78
Brian Niebes <de.dbn@protonmail.com>
2024-04-17 19:02:05 +0200

    IMPROVEMENT - Removed unused import

--- app/composeApp/src/desktopMain/kotlin/de/dbn/oss/timer/TimerDisplay.kt
+++ app/composeApp/src/desktopMain/kotlin/de/dbn/oss/timer/TimerDisplay.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
